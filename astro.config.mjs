c609cb8509c701ab3ef164691fff9fdebf41820f
mastog <mastooooooogway@gmail.com>
2025-02-28 12:31:20 +0800

    feat: add open source astro style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
+// Import the Vercel adapter
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://positivustheme.vercel.app",
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
+});
