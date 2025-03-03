5ac58021bb091fb5cf7f4dba552c0103356c4bb0
SYB <yubo.sha@ucdconnect.ie>
2025-03-03 21:29:51 +0800

    backend development

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()# Load environment variables
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get('DATABASE_URL'))
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    FILE_UPLOAD_DIR = os.path.join(basedir, 'uploaded_File')
+
