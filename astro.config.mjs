16a1730bd1d7384d793a5a52098cf89ba15e50bd
SYB <yubo.sha@ucdconnect.ie>
2025-03-26 23:09:18 +0800

    chore: debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import pluginReact from '@vitejs/plugin-react-refresh';
 
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel';
@@ -9,7 +8,6 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: "https://positivustheme.vercel.app",
-  plugins: [pluginReact()],
   integrations: [tailwind(),react()],
   output: 'server',
   adapter: vercel(),
