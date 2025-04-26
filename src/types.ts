2c1c3d4566af2297cac73f6f1ecd6fef60f9085e
mastog <mastooooooogway@gmail.com>
2025-04-26 20:57:04 +0800

    chore: add grid card

---
+++ src/types.ts
@@ -0,0 +1,46 @@
+export interface ContentItem {
+    title: string;
+    description: string;
+    link: string;
+    avatar: string;
+  }
+  
+  export const GRID1_CONTENT: ContentItem[] = [
+    {
+      title: "Kobe",
+      description: "the father",
+      link: "/example1",
+      avatar: "../src/assets/team/d12.png"
+    },
+    // 添加更多默认内容...
+  ];
+
+  export const GRID2_CONTENT: ContentItem[] = [
+    {
+      title: "Kobe",
+      description: "the cousin",
+      link: "/example1",
+      avatar: "../src/assets/team/d12.png"
+    },
+    // 添加更多默认内容...
+  ];
+
+  export const GRID3_CONTENT: ContentItem[] = [
+    {
+      title: "Kobe",
+      description: "the friend",
+      link: "/example1",
+      avatar: "../src/assets/team/d12.png"
+    },
+    // 添加更多默认内容...
+  ];
+
+  export const GRID4_CONTENT: ContentItem[] = [
+    {
+      title: "Kobe",
+      description: "the co-worker",
+      link: "/example1",
+      avatar: "../src/assets/team/d12.png"
+    },
+    // 添加更多默认内容...
+  ];
